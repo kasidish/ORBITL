cdc2e79bb3eff55dbff7aae0ec07c60d33161156
kasidish <kasidish.b@gmail.com>
2026-06-13 22:19:57 +0700

    gap page

--- web/src/pages/ContactPage.jsx
+++ web/src/pages/ContactPage.jsx
@@ -19,7 +19,7 @@ function ContactPage() {
       <div className="min-h-screen flex flex-col">
         <Header />
 
-        <main className="flex-1 pt-24 pb-16">
+        <main className="flex-1 pt-8 pb-16">
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <motion.div
               initial={{ opacity: 0, y: 20 }}
--- web/src/pages/NewsPage.jsx
+++ web/src/pages/NewsPage.jsx
@@ -43,7 +43,7 @@ function NewsPage() {
       <div className="min-h-screen flex flex-col">
         <Header />
 
-        <main className="flex-1 pt-24 pb-16">
+        <main className="flex-1 pt-8 pb-16">
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <motion.div
               initial={{ opacity: 0, y: 20 }}
